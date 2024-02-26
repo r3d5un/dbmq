d13cc4c0895598656cb8b38083986559b9fde129
r3d5un <r3d5un@pm.me>
2024-02-26 22:57:29 +0100

    Implement GetNext MessageModel method

--- internal/data/messages.go
+++ internal/data/messages.go
@@ -1,9 +1,12 @@
 package data
 
 import (
+	"context"
+	"encoding/json"
 	"log/slog"
 	"time"
 
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
@@ -21,17 +24,47 @@ type MessageModel struct {
 	Pool *pgxpool.Pool
 }
 
-func (m MessageModel) Insert(data Data) (*Message, error) {
+func (m MessageModel) Insert(tx pgx.Tx, data Data) (*Message, error) {
 	slog.Error("not implemented")
 	return nil, nil
 }
 
-func (m MessageModel) GetNext() (*Message, error) {
-	slog.Error("not implemented")
-	return nil, nil
+func (m MessageModel) GetNext(tx pgx.Tx) (*Message, error) {
+	stmt := `SELECT id, data, created_at
+        FROM messages
+        ORDER BY created_at
+        FOR UPDATE SKIP LOCKED
+        LIMIT 1;`
+
+	var msg Message
+	var rawData []byte
+
+	err := tx.QueryRow(context.Background(), stmt).Scan(
+		&msg.ID,
+		&rawData,
+		&msg.CreatedAt,
+	)
+	if err != nil {
+		switch err {
+		case pgx.ErrNoRows:
+			slog.Info("no new messages", "error", err)
+			return nil, nil
+		default:
+			slog.Error("unable to execute statement", "error", err)
+			return nil, err
+		}
+	}
+
+	err = json.Unmarshal(rawData, &msg.Data)
+	if err != nil {
+		slog.Error("unable to unmarshal data", "error", err)
+		return nil, err
+	}
+
+	return &msg, nil
 }
 
-func (m MessageModel) Delete(id int64) error {
+func (m MessageModel) Delete(tx *pgx.Tx, id int64) error {
 	slog.Error("not implemented")
 	return nil
 }
